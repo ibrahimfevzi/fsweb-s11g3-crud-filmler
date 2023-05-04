d017976fbf1ed071004aa004d084b61f5bdeee23
ibrahimfevzi <ibrahimfevzi@gmail.com>
2023-05-04 16:26:09 +0300

    MVP tamamlandı, esnek görevlere bakılacak

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch, Redirect, useHistory } from "react-router-dom";
 import MovieList from "./components/MovieList";
 import Movie from "./components/Movie";
 import EditMovieForm from "./components/EditMovieForm";
+import AddMovieForm from "./components/AddMovieForm";
 
 import MovieHeader from "./components/MovieHeader";
 
@@ -55,6 +56,10 @@ const App = (props) => {
           <Switch>
             <Route path="/movies/edit/:id">
               <EditMovieForm setMovies={setMovies} />
+            </Route>
+
+            <Route path="/movies/add">
+              <AddMovieForm setMovies={setMovies} />
             </Route>
 
             <Route path="/movies/:id">
